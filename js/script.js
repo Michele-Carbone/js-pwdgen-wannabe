db860aee8a36444de6ee24bd61dcf7b9802a6aa4
Michele-Carbone <michele.carboneetlab@gmail.com>
2021-06-30 22:31:38 +0800

    setup

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var name = prompt('Inserisci il tuo nome')
+var lastName = prompt('Inserisci il tuo cognome')
+var color = prompt('Inserisci il tuo colore preferito')
+
+console.log(name);
+console.log(lastName);
+console.log(color);
