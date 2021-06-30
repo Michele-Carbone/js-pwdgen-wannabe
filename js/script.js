674342473acc0a964781015d233729a3b9f7299d
Michele-Carbone <michele.carboneetlab@gmail.com>
2021-06-30 22:57:44 +0800

    account-name

--- js/script.js
+++ js/script.js
@@ -5,3 +5,30 @@ var color = prompt('Inserisci il tuo colore preferito')
 console.log(name);
 console.log(lastName);
 console.log(color);
+
+/*Nome visualizzato in html*/
+var namePlaceholder = document.getElementById('name')
+console.log(namePlaceholder)
+
+namePlaceholder.innerHTML = name;
+
+/*Cognome visualizzato in html*/
+var lastNamePlaceholder = document.getElementById('last-name')
+console.log(lastNamePlaceholder)
+
+lastNamePlaceholder.innerHTML = lastName;
+
+/*Colore visualizzato in html*/
+var colorPlaceholder = document.getElementById('color')
+console.log(colorPlaceholder)
+
+colorPlaceholder.innerHTML = color;
+
+/*Password visualizzato in html*/
+
+var currentYear = parseInt(21);
+var passwordTest = name + lastName + color + currentYear;
+var passwordTest = document.getElementById('pass')
+console.log(passwordTest)
+
+passwordTest.innerHTML = name + lastName + color + currentYear;
